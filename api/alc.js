b0286c2248fa4ddc7e574cf189972d04f2c8293b
PaulB-HTST <paul.bird@harvestingthesuntwice.org>
2026-06-12 14:49:21 +0100

    Update alc.js

--- api/alc.js
+++ api/alc.js
@@ -3,10 +3,14 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate');
 
-  const url = 'https://environment.data.gov.uk/spatialdata/agricultural-land-classification-provisional-england/wfs?service=WFS&version=2.0.0&request=GetFeature&typeNames=Agricultural_Land_Classification_(Provisional)_(England)&outputFormat=application/json&srsName=EPSG:4326&count=2000';
+  // ArcGIS REST API for Provisional ALC England - more reliable than WFS
+  const url = 'https://services.arcgis.com/JJzESW51TqeY9uat/arcgis/rest/services/Provisional_Agricultural_Land_Classification_ALC/FeatureServer/0/query?where=1%3D1&outFields=ALC_GRADE&returnGeometry=true&outSR=4326&f=geojson&resultRecordCount=2000';
 
   try {
-    const response = await fetch(url);
+    const controller = new AbortController();
+    const timeout = setTimeout(() => controller.abort(), 25000);
+    const response = await fetch(url, { signal: controller.signal });
+    clearTimeout(timeout);
     if (!response.ok) throw new Error('Upstream ' + response.status);
     const data = await response.json();
     res.setHeader('Content-Type', 'application/json');
